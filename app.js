5665aec4da5d2fd9c534b3fdfe547885df618da7
Natalie Theodoropoulou <natalie.theo@gmail.com>
2023-07-03 11:02:50 +0100

    finished dom stuff, pic and info is coming up. need to edit css

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const url = 'https://coffee.alexflipnote.dev/random'
+const url = 'https://api.nasa.gov/planetary/apod?api_key='
+const api_key = config.NASA_API_KEY
 
-const fetchNasaPic = async () => {
+const fetchNASAData = async () => {
     try {
-        const response = await fetch(`${url}`)
+        const response = await fetch(`${url}${api_key}`)
         const data = await response.json()
-        console.log('coffee pic', data)
+        console.log('NASA APID data', data)
+        displayData(data)
     }   catch (error) {
         console.log(error)
     }
 }
 
-fetchCoffeePic()
+fetchNASAData()
 
-const displayCoffeePic = data => {
-    document.getElementById('picture').src = 
+const displayData = data => {
+    document.getElementById('title').textContent = data.title
+    document.getElementById('date').textContent = data.date
+    document.getElementById('image').src = data.hdurl
+    document.getElementById('explanation').textContent = data.explanation
+
 }
